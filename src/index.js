1198519f2939b384876847a5d3c8925e9da3a57a
eshevat <shevate@canisius.edu>
2022-11-01 07:56:54 -0400

    Icon JS edit

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ function formatDate(date) {
 }
 
 function displayWeatherCondition(response) {
+  console.log(data)
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -40,7 +41,7 @@ descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(currentTime);
-iconElement.setAttribute("src", "http://openweathermap.org/img/wn/10d@2x.png");
+iconElement.setAttribute("src", "http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png");
 }
 
 function searchCity(city) {
