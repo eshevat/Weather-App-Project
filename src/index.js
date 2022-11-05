2f01afe3698c50b212a823f5850e286f9b3acc14
eshevat <shevate@canisius.edu>
2022-11-05 08:43:53 -0400

    Edited js functions format to previous format

--- src/index.js
+++ src/index.js
@@ -24,26 +24,17 @@ function formatDate(date) {
 }
 
 function displayWeatherCondition(response) {
-  console.log(data);
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let iconElement = document.querySelector("#icon");
-  let dateElement = document.querySelector("#date");
-  let currentTime = new Date();
-
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.weather[0].main;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(currentTime);
-  iconElement.setAttribute(
-    "src",
-    "http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png"
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
   );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
 }
 
 function searchCity(city) {
@@ -90,7 +81,14 @@ fahrenheitTemperature.addEventListener("click", convertToFahrenheit);
 let celsiusTemperature = document.querySelector("#celsius");
 celsiusTemperature.addEventListener("click", convertToCelsius);
 
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("click", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("New York");
